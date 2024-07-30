b9d89e311a06830a5523055480813817922ee637
Pedro Quadros <p.quadros.a@gmail.com>
2024-07-30 11:10:25 -0300

    Criamos as mensagens erro e implementamos programação defensiva

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,8 +15,8 @@ public class ChessMatch {
     }
 
     public ChessPiece[][] getPieces(){
-       ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumns()];
-       for(int i = 0; i < board.getRow(); i++){
+       ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+       for(int i = 0; i < board.getRows(); i++){
            for(int j = 0; j < board.getColumns(); j++){
                mat[i][j] = (ChessPiece) board.piece(i, j);
            }
@@ -26,7 +26,7 @@ public class ChessMatch {
 
     private void initialSetup(){
         board.placePiece(new Rook(board, Color.WHITE), new Position(7,7));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.BLACK), new Position(1, 4));
         board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 }

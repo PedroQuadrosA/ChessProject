ced3cf3ec37d37f27dfcb12f17df1c7906cea9d9
Pedro Quadros <p.quadros.a@gmail.com>
2024-08-02 10:33:20 -0300

    Criamos a movimentação base das peças de uma origem para um destino

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,6 +43,19 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece temp = piece(position);
+        temp.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return temp;
+    }
+
     private boolean positionExists(int row, int column){
        return row >= 0 && row < rows && column >= 0 && column < columns;
     }

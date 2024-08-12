56891685fa5b34551b71a22b15b7f00682c2662d
Pedro Quadros <p.quadros.a@gmail.com>
2024-08-12 11:00:18 -0300

    ADicionamos duas listas para armazenar as peças capturadas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,15 +6,22 @@ import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
     private Board board;
     private int turn;
     private Color currentPlayer;
 
+    private List<Piece> piecesOnTheBoard;
+    private List<Piece> capturedPieces = new ArrayList<>();
+
     public ChessMatch(){
         board = new Board(8, 8);
         turn = 1;
         currentPlayer = Color.WHITE;
+        piecesOnTheBoard = new ArrayList<>();
         initialSetup();
     }
 
@@ -54,6 +61,10 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -84,6 +95,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column,int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup(){
